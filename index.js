08812ce37eb8cd1a9102c55a6d3cae1ac584b935
Dean Landolt <dean@deanlandolt.com>
2015-04-22 22:07:39 -0400

    Unify route handling across http and socket requests

--- index.js
+++ index.js
@@ -7,17 +7,10 @@ var split = require('split');
 var url = require('url');
 var xtend = require('xtend');
 
-function notFound(request) {
-  // TODO: custom Error instance
-  var error = new Error('Not Found: ' + request.url);
-  error.status = 400;
-  return error;
-}
-
 //
 // garbage little utils for routes (as a stopgap until we gets better routing)
 //
-function parseRoutes(api, versions) {
+function parseEndpoints(api, versions) {
   versions || (versions = {});
   var routes = versions[api.version || '*'] = versions[api.version || '*'] = {};
 
@@ -48,7 +41,7 @@ function parseRoutes(api, versions) {
     // parse previous api routes if provided
     //
     if (api.previous) {
-      parseRoutes(api.previous, versions)
+      parseEndpoints(api.previous, versions)
     }
   }
 
@@ -56,30 +49,39 @@ function parseRoutes(api, versions) {
 }
 
 //
+// 404 error class
+//
+function NotFoundError(request) {
+  this.status = 404;
+  this.message = 'Not Found: ' + request.url;
+  this.stack = Error().stack;
+}
+NotFoundError.prototype = Object.create(Error.prototype);
+NotFoundError.prototype.name = 'NotFoundError';
+NotFoundError.prototype.constructor = NotFoundError
+
+//
 // crappy little default router that parsers semver from url paths
 //
 function defaultRouter(api) {
-  var routes = parseRoutes(api);
+  var versions = parseEndpoints(api);
 
   return function (request) {
-
     var path = request.url;
     var components = url.parse(path).pathname.split('/').slice(1);
     var candidates = {};
 
-    var range = decodeURIComponent(components.shift())
+    var range = decodeURIComponent(components.shift());
     var validRange = semver.validRange(range);
-    try {
-      Object.keys(routes).forEach(function (version) {
-        if (semver.satisfies(version, validRange)) {
-          candidates[version] = routes[version];
-        }
-      });
-    }
-    catch (e) {
-      // TODO: rethrow for invalid version?
-      return null;
-    }
+
+    //
+    // find a candidate set of allowable versions for route matching
+    //
+    Object.keys(versions).forEach(function (version) {
+      if (semver.satisfies(version, validRange)) {
+        candidates[version] = versions[version];
+      }
+    });
 
     var match = paramify('/' + components.join('/'));
 
@@ -95,83 +97,101 @@ function defaultRouter(api) {
         // match route by method and path info
         //
         if (request.method == endpoint.method && match(route)) {
-          return [ endpoint, match ];
+          //
+          // add params to request and return match context
+          //
+          request.params = match.params;
+          return {
+            request: request,
+            handler: endpoint.handler
+          };
         }
       }
     }
-  }
+
+    throw new NotFoundError(request);
+  };
 }
 
 var defaultHeaders = { 'content-type': 'application/json' };
 
-//
-// stupid simple server, invokes request handlers, expects stream or JSON body
-//
-module.exports = function serve (config, callback) {
-  var router = defaultRouter(config.api);
+exports.serve = function serve(config) {
   var baseHeaders = xtend(config.headers, defaultHeaders);
+  var router = defaultRouter(config.api);
 
-  function getHeaders(data) {
-    return xtend(data.headers, baseHeaders);
+  //
+  // logic for identifying and invoking the correct endpoint
+  //
+  function process(request) {
+    //
+    // resolve as promise for error trapping
+    // resolves to request for convenient chaining
+    //
+    return Promise.resolve(request)
+      .then(router)
+      .then(function (route) {
+        return route.handler(route.request);
+      })
+      .then(exports.normalizeResponse)
   }
 
-  function routeRequest(request, response) {
+  function handleRequest(request, response) {
 
-    function writeResponse(data) {
-      response.writeHead(data.status || 200, getHeaders(data));
-      response.end(JSON.stringify(data.body)); 
+    function writeJSON(data) {
+      var headers = xtend(data.headers, baseHeaders);
+      response.writeHead(data.status || 200, headers);
+      response.end(JSON.stringify(data.body, null, '  '));
     }
 
     function writeError(error) {
       //
-      // normalize errors as responses
+      // normalize errors as JSON responses
       //
-      error.status || (error.status = 500);
-      error.body || (error.body = error.message);
-      writeResponse(error);
+      writeJSON({
+        status: error.status || 500,
+        body: {
+          name: error.name,
+          message: error.message,
+          // TODO: only write stack in dev mode
+          stack: error.stack
+        }
+      });
     }
 
     //
     // look up handler for request
     //
-    var route = router(request);
-    if (!route) {
-      return writeError(notFound(request));
-    }
-
-    //
-    // add match params to request and invoke handler
-    //
-    request.params = route[1].params;
-    Promise.resolve(route[0].handler(request)).then(function (data) {
+    return process(request).then(function (data) {
+      if (typeof data.pipe !== 'function') {
+        //
+        // write non-stream data as JSON response
+        //
+        return writeJSON(data);
+      }
+      //
+      // extract metadata from own-props on stream, if available
+      //
+      var status = data.hasOwnProperty('status') && data.status;
+      var headers = data.hasOwnProperty('headers') && data.headers;
+      response.writeHead(status || 200, xtend(headers, baseHeaders));
 
       //
-      // stream bodies get piped directly to response, passing through metadata
+      // stream responses get piped to response stream
       //
-      if (typeof data.pipe === 'function') {
-        response.writeHead(data.status || 200, getHeaders(data));
-        data.pipe(response)
-      }
-      else {
-        writeResponse(data);
-      }
-
-    }).catch(writeError);
+      data.pipe(response);
+    })
+    .catch(writeError)
   }
 
   //
   // handler for websocket requests
   //
-  function routeEvent(stream) {
+  function handleEvent(stream) {
 
     //
     // helper for normalizing and serializing event data
     //
-
-    function handleEvent(request) {
-      var event = {
-        url: request.url
-      };
+    function handleRequestEvent(request) {
 
       function writeEvent(data) {
         stream.write(JSON.stringify(xtend(event, data)) + '\n');
@@ -181,23 +201,14 @@ module.exports = function serve (config, callback) {
         writeEvent({ error: error });
       }
 
-      //
-      // run event request payload through standard request routing
-      //
-      var route = router(request);
-
-      if (!route) {
-        return writeEvent({
-          url: request.url,
-          error: notFound(request)
-        });
-      }
+      var event = {
+        url: request.url
+      };
 
       //
-      // add match params to request and invoke handler
+      // run event request payload through standard request processing
       //
-      request.params = route[1].params;
-      Promise.resolve(route[0].handler(request)).then(function (data) {
+      return process(request).then(function (data) {
 
         //
         // non-streaming handler responses result in a single event record
@@ -247,20 +258,36 @@ module.exports = function serve (config, callback) {
         console.error('ERROR: ' + error);
       })
       .pipe(split(JSON.parse))
-      .on('data', handleEvent);
+      .on('data', handleRequestEvent);
   }
 
   //
   // spin up http server to handle requests
   //
-  var server = http.createServer(routeRequest);
+  var server = http.createServer(handleRequest);
 
-  server.listen(config.port, config.host, callback);
+  return Promise.resolve(null).then(function () {
+    server.listen(config.port, config.host, function (error) {
+      if (error) {
+        throw error;
+      }
 
-  //
-  // attach websocket server if requested
-  //
-  if (config.socketPath) {
-    engine(routeEvent).attach(server, config.socketPath);
-  }
+      //
+      // attach websocket server if requested
+      //
+      if (config.socketPath) {
+        engine(handleEvent).attach(server, config.socketPath);
+      }
+
+      return server;
+    });
+  });
+}
+
+//
+// all response normalization/validation logic should happen in one place
+//
+exports.normalizeResponse = function (data) {
+  // TODO
+  return data;
 };
